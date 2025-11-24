6bf4e55f63a086e10b5949687b0576d65e4c81b2
isayaksh <isayaksh@gmail.com>
2025-11-24 20:39:46 +0900

    add global exception handler

---
+++ src/main/kotlin/com/minturl/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,37 @@
+package com.minturl.handler
+
+import com.minturl.common.response.ApiResponse
+import com.minturl.common.response.ErrorCode
+import com.minturl.common.response.ResponseBuilder
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingPathVariableException
+import org.springframework.web.bind.MissingServletRequestParameterException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun handleTypeMismatch(ex: MethodArgumentTypeMismatchException): ResponseEntity<ApiResponse<Nothing?>> {
+        return ResponseBuilder.error(HttpStatus.BAD_REQUEST, ErrorCode.INVALID_REQUEST);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException::class)
+    fun handleMissingParam(ex: MissingServletRequestParameterException): ResponseEntity<ApiResponse<Nothing?>> {
+        return ResponseBuilder.error(HttpStatus.BAD_REQUEST, ErrorCode.INVALID_REQUEST);
+    }
+
+    @ExceptionHandler(MissingPathVariableException::class)
+    fun handleMissingPath(ex: MissingPathVariableException): ResponseEntity<ApiResponse<Nothing?>> {
+        return ResponseBuilder.error(HttpStatus.BAD_REQUEST, ErrorCode.INVALID_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidation(ex: MethodArgumentNotValidException): ResponseEntity<ApiResponse<Nothing?>> {
+        return ResponseBuilder.error(HttpStatus.BAD_REQUEST, ErrorCode.INVALID_REQUEST);
+    }
+}
